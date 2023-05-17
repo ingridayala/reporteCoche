37f3ee930cef1a310e6a06ce94af44028910655d
ingridayala <ingridayala414@gmail.com>
2023-05-17 13:41:55 +0200

    añadir conductores

--- pages/reporteCoche.php
+++ pages/reporteCoche.php
@@ -50,21 +50,21 @@ $conexion = $bbdd->connect();
     </form>
     
     <?php
-include_once('../includes/conexion.php');
+      include_once('../includes/conexion.php');
 
-if($_SERVER["REQUEST_METHOD"] == "POST"){
-    $conexion = new conexion();
-    $con = $conexion->connect();
+      if($_SERVER["REQUEST_METHOD"] == "POST"){
+          $conexion = new conexion();
+          $con = $conexion->connect();
 
-    $nombre = $_POST['nombre'];
-    $dni = $_POST['dni'];
-    $alta_baja = $_POST['alta_baja'];
+          $nombre = $_POST['nombre'];
+          $dni = $_POST['dni'];
+          $alta_baja = $_POST['alta_baja'];
 
-    $sql = "INSERT INTO conductores (nombre, dni, alta_baja) VALUES (?, ?, ?)";
-    $stmt= $con->prepare($sql);
-    $stmt->execute([$nombre, $dni, $alta_baja]);
-}
-?>
+          $sql = "INSERT INTO conductores (nombre, dni, alta_baja) VALUES (?, ?, ?)";
+          $stmt= $con->prepare($sql);
+          $stmt->execute([$nombre, $dni, $alta_baja]);
+      }
+    ?>
     
 
 <script src="app.js"></script>
