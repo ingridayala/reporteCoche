a371cff52f3e70d7132d2a645e62adbc7e4bfa46
ingridayala <ingridayala414@gmail.com>
2023-05-29 16:15:32 +0200

    muestra los daños con el conductor correcto

--- pages/reporteCoche.php
+++ pages/reporteCoche.php
@@ -47,57 +47,81 @@ $conexion = $bbdd->connect();
                         GROUP BY vehiculo_idvehiculo
                     ) hv
                     JOIN taller.historial_cv h ON hv.vehiculo_idvehiculo = h.vehiculo_idvehiculo AND hv.max_idhistorial = h.idhistorial
-                    JOIN taller.conductor c ON h.conductor_idconductor = c.idconductor
                     JOIN taller.vehiculo v ON h.vehiculo_idvehiculo = v.idvehiculo
                     LEFT JOIN taller.revisiones r ON v.idvehiculo = r.vehiculo_idvehiculo
+                    LEFT JOIN taller.conductor c ON r.conductor_idconductor = c.idconductor
                     LEFT JOIN taller.incidencia i ON r.idrevisiones = i.revisiones_idrevisiones
                     LEFT JOIN taller.parte p ON i.parte_idparte = p.idparte
                     JOIN taller.licencia l ON v.licencia_idlicencia = l.idlicencia
-                    WHERE l.n_licencia = ?;
+                    WHERE l.n_licencia = ?
                     ");
 
                     // Execute statement
                     $stmt->execute([$n_licencia]);
 
                     $resultados = $stmt->fetchAll();
-                
-                    foreach ($resultados as $resultado) {
-                        echo '<div class="card mb-3">';
-                        echo '<div class="card-body">';
-                        echo '<h5 class="card-title">Matrícula del vehículo: ' . $resultado['matricula'] . '</h5>';
-                        echo '<p class="card-text">Nombre del conductor: ' . $resultado['nombre_apellidos'] . '</p>';
-                        echo '<p class="card-text">Descripción del daño: ' . $resultado['descripcion_danio'] . '</p>';
-                        echo '<p class="card-text">Código de parte dañada: ' . $resultado['codigo'] . '</p>';
-                        echo '</div>';
-                        echo '</div>';
-                    }
-                    
-                    // Create an array to represent the grid
-                    $grid = array_fill(0, 6, array_fill(0, 9, ' '));
-                    foreach ($resultados as $resultado) {
-                        $parte = $resultado['codigo']; // e.g., 'B2'
-                        $fila = (int) $parte[1] - 1; // second character converted to zero-based index
-                        $columna = ord($parte[0]) - ord('A'); // first character converted to zero-based index
-                        $grid[$fila][$columna] = 'X';
-                    }
+                     
+                    // Crea un arreglo para agrupar los daños por matrícula
+                $vehiculos = [];
 
-                    // Print the grid
-                    echo '<div class="grid-container" style="background-image: url(\'../assets/img/diagramaCoche.jpg\');">';
-                    foreach ($grid as $fila => $row) {
-                        foreach ($row as $columna => $cell) {
-                            if ($cell == 'X') {
-                                echo '<div class="damage-indicator" style="grid-row: ' . ($fila + 1) . '; grid-column: ' . ($columna + 1) . ';"></div>';
-                            }
-                        }
+                foreach ($resultados as $resultado) {
+                    $matricula = $resultado['matricula'];
+                    if (!isset($vehiculos[$matricula])) {
+                        // Si este vehículo aún no está en el arreglo, agrégalo
+                        $vehiculos[$matricula] = [
+                            'nombre_apellidos' => $resultado['nombre_apellidos'],
+                            'descripcion_danio' => $resultado['descripcion_danio'],
+                            'danios' => []
+                        ];
+                    }
+                    // Agrega el daño a la lista de daños para este vehículo
+                    $vehiculos[$matricula]['danios'][] = [
+                        
+                        'codigo' => $resultado['codigo']
+                    ];
+                }
+
+                // Ahora imprime los resultados
+                foreach ($vehiculos as $matricula => $vehiculo) {
+                    echo '<div class="card mb-3">';
+                    echo '<div class="card-body">';
+                    echo '<h5 class="card-title">Matrícula del vehículo: ' . $matricula . '</h5>';
+                    echo '<p class="card-text">Nombre del conductor: ' . $vehiculo['nombre_apellidos'] . '</p>';
+                    echo '<p class="card-text">Descripción del daño: ' . $vehiculo['descripcion_danio'] . '</p>';
+                    foreach ($vehiculo['danios'] as $dano) {
+                        
+                        echo '<p class="card-text">Código de parte dañada: ' . $dano['codigo'] . '</p>';
                     }
                     echo '</div>';
-                } catch(PDOException $e) {
-                    echo "Error: " . $e->getMessage();
+                    echo '</div>';
                 }
+
+                // Create an array to represent the grid
+                $grid = array_fill(0, 6, array_fill(0, 9, ' '));
+                foreach ($resultados as $resultado) {
+                    $parte = $resultado['codigo']; // e.g., 'B2'
+                    $fila = (int) $parte[1] - 1; // second character converted to zero-based index
+                    $columna = ord($parte[0]) - ord('A'); // first character converted to zero-based index
+                    $grid[$fila][$columna] = 'X';
+                }
+
+                // Print the grid
+                echo '<div class="grid-container" style="background-image: url(\'../assets/img/diagramaCoche.jpg\');background-size: 100%;">';
+                foreach ($grid as $fila => $row) {
+                    foreach ($row as $columna => $cell) {
+                        if ($cell == 'X') {
+                            echo '<div class="damage-indicator" style="grid-row: ' . ($fila + 1) . '; grid-column: ' . ($columna + 1) . ';"></div>';
+                        }
+                    }
+                }
+                echo '</div>';
+            } catch(PDOException $e) {
+                echo "Error: " . $e->getMessage();
             }
+        }
         ?>
     </form>
-</div>
+    </div>
 
         <form class="contain-form" action="addConductor.php" method="post">
             <label for="nombre">Nombre:</label><br>
